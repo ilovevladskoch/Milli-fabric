cdaee453fe8d1d5a8b140dbcc12b01c763b0ab89
ilovevladskoch <skochipec@yandex.ru>
2022-09-08 23:54:57 +0300

    update

--- js/slider.js
+++ js/slider.js
@@ -54,4 +54,64 @@ dots.forEach((item, indexDot) => {
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
 
-setInterval(nextSlide, 2500);
+setInterval(nextSlide, 3000);
+
+
+const prevBtnCustomer = document.querySelector('.btn__prev__customer'),
+        nextBtnCustomer = document.querySelector('.btn__next__customer'),
+        slidesCustomer = document.querySelectorAll('.slider__item__customer'),
+        dotsCustomer = document.querySelectorAll('.dot__customer');
+
+let indexCustomer = 0;
+
+const activeSlideCustomer = n => {
+    console.log(n);
+    for(slideCustomer of slidesCustomer) {
+        slideCustomer.classList.remove('active__slider__item__customer');
+    }
+    slidesCustomer[n].classList.add('active__slider__item__customer')
+}
+
+const activeDotCustomer = n => {
+    for(dot of dotsCustomer) {
+        dot.classList.remove('active__dot__customer');
+    }
+    dotsCustomer[n].classList.add('active__dot__customer');
+}
+
+const prepareCurrentSlideCustomer = ind => {
+    activeSlideCustomer(ind);
+    activeDotCustomer(ind);
+}
+
+const nextSlideCustomer = () => {
+    if(index == slidesCustomer.length -1) {
+        index = 0;
+        prepareCurrentSlideCustomer(index);
+    } else {
+        index++;
+        prepareCurrentSlideCustomer(index);
+    }
+}
+
+const prevSlideCustomer = () => {
+    if(index == 0) {
+        index = slidesCustomer.length -1;
+        prepareCurrentSlideCustomer(index);
+    } else {
+        index--;
+        prepareCurrentSlideCustomer(index);
+    }
+}
+
+dotsCustomer.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlideCustomer(index);
+    })
+}) 
+
+nextBtnCustomer.addEventListener('click', nextSlideCustomer);
+prevBtnCustomer.addEventListener('click', prevSlideCustomer);
+
+setInterval(nextSlideCustomer, 3000);
